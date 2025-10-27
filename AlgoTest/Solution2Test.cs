0a216982844402a80ee8507701b5e6cd7d2dc88d
jenkins <shuitai504@126.com>
2025-10-27 20:57:38 +0800

    add solution2

---
+++ AlgoTest/Solution2Test.cs
@@ -0,0 +1,56 @@
+﻿using Algorithm;
+
+namespace AlgoTest;
+
+public class Solution2Test
+{
+    [Test]
+    public void Test1()
+    {
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var solution = new Solution2();
+        var result = solution.AddTwoNumbers(l1, l2);
+        Assert.That(result.val, Is.EqualTo(7));
+        Assert.That(result.next.val, Is.EqualTo(0));
+        Assert.That(result.next.next.val, Is.EqualTo(8));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        var l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        var solution = new Solution2();
+        var result = solution.AddTwoNumbers(l1, l2);
+        Assert.That(result.val, Is.EqualTo(8));
+        Assert.That(result.next.val, Is.EqualTo(9));
+        Assert.That(result.next.next.val, Is.EqualTo(9));
+        Assert.That(result.next.next.next.val, Is.EqualTo(9));
+        Assert.That(result.next.next.next.next.val, Is.EqualTo(0));
+        Assert.That(result.next.next.next.next.next.val, Is.EqualTo(0));
+        Assert.That(result.next.next.next.next.next.next.val, Is.EqualTo(0));
+        Assert.That(result.next.next.next.next.next.next.next.val, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var l1 = new ListNode(0);
+        var l2 = new ListNode(0);
+        var solution = new Solution2();
+        var result = solution.AddTwoNumbers(l1, l2);
+        Assert.That(result.val, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Test4()
+    {
+        var l1 = new ListNode(5);
+        var l2 = new ListNode(5);
+        var solution = new Solution2();
+        var result = solution.AddTwoNumbers(l1, l2);
+        Assert.That(result.val, Is.EqualTo(0));
+        Assert.That(result.next.val, Is.EqualTo(1));
+    }
+}
---
+++ Algorithm/Solution2.cs
@@ -0,0 +1,60 @@
+﻿namespace Algorithm;
+
+// https://leetcode.cn/problems/add-two-numbers/description/
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution2
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        var head = new ListNode();
+        var current = head;
+        var isCarry = 0;
+        
+        while (l1 != null || l2 != null)
+        {
+            var newValue = 0;
+            if (l1 == null)
+            {
+                newValue = (l2.val + isCarry) % 10;
+                isCarry = (l2.val + isCarry) / 10;
+                
+                l2 = l2.next;
+            }
+            else if (l2 == null)
+            {
+                newValue = (l1.val + isCarry) % 10;
+                isCarry = (l1.val + isCarry) / 10;
+                
+                l1 = l1.next;
+            }
+            else
+            {
+                newValue = (l1.val + l2.val + isCarry) % 10;
+                isCarry = (l1.val + l2.val + isCarry) / 10;
+                
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            current.next = new ListNode(newValue);
+            current = current.next;
+        }
+        
+        if (isCarry > 0)
+        {
+            current.next = new ListNode(isCarry);
+        }
+
+        return head.next;
+    }
+}
